3b8a48f60438796ce3f463d796e2e49431c47ffc
Pavel Kucherov <26567338+pkucherov@users.noreply.github.com>
2020-10-15 19:46:34 +0200

    Initial commit

---
+++ Source/FPSProject/FPSProjectGameModeBase.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "FPSProjectGameModeBase.h"
+#include "FPSProject.h"
+
+void AFPSProjectGameModeBase::StartPlay()
+{
+    Super::StartPlay();
+
+    if (GEngine)
+    {
+        // Display a debug message for five seconds. 
+        // The -1 "Key" value (first argument) indicates that we will never need to update or refresh this message.
+        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is FPSGameMode!"));
+    }
+}
